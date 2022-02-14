2c26c87ca70071dbe9d9892fbcf48476c335e72f
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-14 13:21:07 +0330

    constructor

--- Class_beginner.py
+++ Class_beginner.py
@@ -1,20 +1,18 @@
 class Point():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     def getX(self):
         return self.x
 
 #build first product from factory
-point1 = Point()
+#point1 -> x = 5, y = 10
+point1 = Point(5, 10)
 #build seccond product from factory
-point2 = Point()
-
-# point1 -> [x=5]
-point1.x = 5
-#point2 -> [x=10]
-point2.x = 10
+#point2 -> x = 1, y = 2
+point2 = Point(1, 2)
 
 print(point1.getX())
-
-#point1 -> self
-#getX -> return self.x -> return point1.x -> 5
-
 print(point2.getX())
