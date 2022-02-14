aae02b6b31c41daa3f58207e8ab0acd4f71dc3ec
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-14 14:34:12 +0330

    obj to str 2

--- obj_to_str.py
+++ obj_to_str.py
@@ -14,7 +14,13 @@ class Point:
     def distanceFromOrigin(self):
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
+    def __str__(self):
+        return f'Point ({self.x}, {self.y})'
+
 p = Point(7,6)
 #if we run p as result we get this
 #<__main__.Point object at 0x10d197c10>
 print(p)
+
+p2 = Point(-1,4)
+print(p2)
