ec0bf6d15d96c576dd752b3b895d015aa3ff5ca9
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-14 13:56:42 +0330

    Class exc 2

--- Class_exc.py
+++ Class_exc.py
@@ -4,12 +4,23 @@ states = ['MI', 'MI', 'PA', 'PA', 'NY']
 
 city_tuples = zip(cityName, populations, states)
 
-
+class City:
+    def __init__(self, n, p, s):
+        self.name = n
+        self.population = p
+        self.state = s
+    def __str__(self):
+        return f'city name: {self.name}, state name: {self.state}, pop: {self.population}'
 
 
 cities = []
 for city_tup in city_tuples:
     name, pop, state = city_tup
-    print(city_tup)
-    print(name, pop, state)
-    print('---')
+
+    #name -> __init__(n)
+    #pop -> __init__(p)
+    #state -> __init__(s)
+    #city is instance of the City class
+    #city -> product City -> factory
+    city = City(name,pop,state)
+    print(city)
