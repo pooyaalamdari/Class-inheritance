f80348157054a07922733807841a67ee0e5c0d4a
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-14 21:23:56 +0330

    inheritance 2

--- test.py
+++ test.py
@@ -10,5 +10,23 @@ class Person:
     def __str__(self):
         return f'{self.name} : {self.getAge()}'
 
-john = Person('John Doe', 1996)
+#want to build new class thas has all Person class
+#features plus new own feature
+
+class Student(Person):
+    def __init__(self, name, born):
+        Person.__init__(self, name, born)
+        self.knowledge = 0
+
+    #def getAge applied from Person
+    #def __str__ applied from Person
+
+    def study(self):
+        self.knowledge += 1
+
+john = Student('John Doe', 1996)
+john.study()
+print(john.knowledge)
+
+#if we don't call knowledge it automate runs Person class
 print(john)
