1ebac1211e4ff395ab9c41474705b3e5ab2c5880
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-14 22:53:46 +0330

    inheritance class and method2

--- test2.py
+++ test2.py
@@ -16,12 +16,20 @@ class EBook(Book):
         Book.__init__(self, title, author)
         self.size = size
 
+class Library:
+    def __init__(self):
+        self.books = []
+    def addBook(self, book):
+        self.books.append(book)
+    def getNumBooks(self):
+        return len(self.books)
+
 my_EBook = EBook('the Odyssey', 'Homer', 2)
 my_PaperBook = PaperBook('the Odyssey', 'Homer', 500)
 
 
-print(my_EBook)
-print(my_EBook.size)
-print('---')
-print(my_PaperBook)
-print(my_PaperBook.numpages)
+all_Books = Library()
+all_Books.addBook(PaperBook)
+all_Books.addBook(EBook)
+
+print(all_Books.getNumBooks())
